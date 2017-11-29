ac129020f964b759c3282c0fffef47be639ccdbc
Jaedon Tan <naseelemak@gmail.com>
2017-11-29 16:47:03 +0800

    Moving to BitBucket

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php include 'header.php';?>
 
-
 <body>
-    <h1>Hello, world!</h1>
-</body>
+    <div class="container">
+        <div class="row">
+        
+        
+            <div class="col-2">
+        
+                <H3>Options</H3>
+                <ul>
+                <li><a href=#>Home</li>
+                </ul>
+                
+            </div>
+        
+            <div class="col-10">
+        
+            
+                <h1>Hello, world!</h1>
 
+        
+            </div>
+    
+        </div>
+
+</div>
+</body>
+    
 <?php include 'footer.php';?>
