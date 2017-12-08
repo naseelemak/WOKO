4711e337329198c4e1050b412038614f6a12d157
Jaedon Tan <naseelemak@gmail.com>
2017-12-08 14:39:30 +0800

    Edit Profile page is now called Register page again. Added fields to the page.

--- registration/registration.php
+++ registration/registration.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>WOKO | Profile</title>
+        <title>WOKO | Register</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -42,29 +42,38 @@
         <br>
 
     </head>
+    
+    <!- Unique code begins here ->
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-md-3"></div>
                 <div class="col-md-6">
-                    <h1>Edit Profile</h1>
+                    <h1>Register</h1>
                     <br>
                     <form>
                         <div class="form-row">
+
+                            <!- TP Number ->
                             <div class="form-group col-md-6">
-                                <label for="inputTP">Webspace Email</label>
-                                <input type="id" class="form-control" id="inputTP" placeholder="TP Number">
+                                <label for="inputTP">TP Number</label>
+                                <input type="id" class="form-control" id="inputTP" placeholder="TPxxxxxx">
                             </div>
+
+                            <!- Password ->
                             <div class="form-group col-md-6">
                                 <label for="inputPassword">Password</label>
                                 <input type="password" class="form-control" id="inputPassword" placeholder="Password">
                             </div>
                         </div>
+
+                        <!- Name ->
                         <div class="form-group">
-                            <label for="inputName">Nickname</label>
-                            <input type="text" class="form-control" id="inputName" placeholder="What you prefer to be called">
+                            <label for="inputName">Name</label>
+                            <input type="text" class="form-control" id="inputName" placeholder="As per APU registration">
                         </div>
-                        
+
+                        <!- Course of Study ->
                         <div class="form-group">
                             <label for="courseSelect">Course of Study</label>
                             <select class="form-control" id="courseSelect">
@@ -77,18 +86,49 @@
                             </select>
                         </div>
 
+                        <!- Email ->
+                        <div class="form-group">
+                            <label for="inputEmail">Email</label>
+                            <input type="email" class="form-control" id="inputEmail" placeholder="Preferred email address">
+                        </div>
+
+                        <!- Phone Number ->
                         <div class="form-group">
                             <label for="inputTel">Phone Number</label>
                             <input class="form-control" type="tel" placeholder="0123456789" id="inputTel">
                         </div>
-                        
+
+                        <!- Preferred Type of Competition ->
                         <div class="form-group">
-                            <div class="form-group col-md-6">
-                                <label for="inputCity">City</label>
-                                <input type="text" class="form-control" id="inputCity">
-                            </div>
-                            
+                            <label for="compTypeSelect">Preferred Type of Competition</label>
+                            <select class="form-control" id="courseSelect">
+                                <option>--Select--</option>
+                                <option>Pitching</option>
+                                <option>Hackathon</option>
+                                <option>Exhibition</option>
+                                <option>Other</option>
+                            </select>
                         </div>
+
+                        <!- Skills ->
+                        <div class="form-group">
+                            <label for="inputSkills">Skills</label>
+                            <input type="text" class="form-control" id="inputSkills">
+                        </div>
+
+                        <!- Interests ->
+                        <div class="form-group">
+                            <label for="inputInterests">Interests</label>
+                            <input type="text" class="form-control" id="inputInterests">
+                        </div>
+
+                        <!- Self-Introduction ->
+                        <div class="form-group">
+                            <label for="inputIntroduction">Self-Introduction (Optional)</label>
+                            <textarea class="form-control" id="inputIntroduction" placeholder="I excel in programming and in identifying different breeds of cats." rows="5"></textarea>
+                        </div>
+
+                        <br>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
