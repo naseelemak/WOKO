47a1e5b42ac1011a1fdecea5a588fe3e28086081
Jaedon Tan <naseelemak@gmail.com>
2017-12-14 19:24:25 +0800

    Added daterangepicker. Added cards for Competitions page.

---
+++ competitions.php
@@ -0,0 +1,46 @@
+<?php 
+
+$currentPage = "Competitions";
+
+include 'misc/header.php';
+
+?>
+
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-12 col-lg-3 mb-4">
+            <p class="text-muted">Search Filters</p>
+
+            <!-- Search field -->
+            <input class="form-control form-control-sm" type="text" placeholder="Title / Author">
+        </div>
+
+        <!-- Competition Cards -->
+        <div class="col-md-12 col-lg-9">
+            <p class="text-muted">23 Competitions Found</p>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-1 card-date">
+                            <p class="card-date-month"><strong>DEC</strong></p>
+                            <p class="card-date-day"><strong>21</strong></p>
+                        </div>
+                        <div class="col-11">
+                            <h4 class="card-title">Card title</h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+
+include 'misc/footer.php';
+
+?>
