751bf053ddbc7b08b439a98c645440825b535a20
Jaedon Tan <naseelemak@gmail.com>
2017-12-15 18:26:28 +0800

    Added login page. Improved Competition card design.

---
+++ login.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>WOKO</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+<body>
+    <div class="container mt-5">
+        <br><br>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <h2 class="text-center"><strong>WOKO</strong></h2>
+                <hr>
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="tpNumber" aria-describedby="emailHelp" placeholder="TP number">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="passwordInput" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block mb-2">Log In</button>
+                    <div class="form-check">
+                        <p><label class="form-check-label">
+                        <input type="checkbox" class="form-check-input">
+                        Remember me
+                    </label>
+                            <a href="#" style="float: right;" href="#">Forgot password?</a></p>
+                    </div>
+
+                </form>
+
+                <!- Create new account ->
+                <small><a href="registration.php" class="text-muted" role="button">Create an account >></a></small>
+
+            </div>
+        </div>
+
+
+    </div>
+    <div class="col-md-4"></div>
+
+</body>
