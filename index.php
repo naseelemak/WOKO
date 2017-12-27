628de68ed5b6788eebf72940726f356dcb74ad21
Jaedon Tan <naseelemak@gmail.com>
2017-12-27 19:21:44 +0800

    Improved home screen. Made tips clickable.

--- index.php
+++ index.php
@@ -6,12 +6,13 @@ include 'misc/header.php';
 
 ?>
 
-<div class="homepage text-white mb-4">
+
+<div class="homepage bg-homepage-head text-white mb-4">
     <div class="container homepage-head">
         <h1 class="homepage-title">Collaborate and Compete</h1>
         <h5 class="homepage-subtitle">Put what you know to the test. Join a competition today.</h5>
 
-        <p>Login to begin, or register first if you haven't.</p>
+        <h6 class="homepage-subtitle-btn">Login to begin, or register first if you haven't.</h6>
 
         <div class="homepage-btn-area">
             <!-- Login Button -->
@@ -25,25 +26,116 @@ include 'misc/header.php';
         </div>
 
 
-        <ul class="homepage-contact-area">
-            <li class="homepage-contact-item">
+        <ul class="homepage-tips-area">
+            <a href="contact.php" class="homepage-tips-item">
+                <li>
+                    <hr class="homepage-line">
+                    <div>
+                        <h3 class="homepage-tips-title">Contact Us</h3>
+                        <p class="homepage-tips-description">Want your competition featured on WOKO? Have questions to ask? Message us.</p>
+                    </div>
+                </li>
+            </a>
+
+            <a href="about.php" class="homepage-tips-item">
+            <li>
                 <hr class="homepage-line">
                 <div>
-                    <h3 class="homepage-contact-title">&lt; Contact Us ></h3>
-                    <p class="homepage-contact-description">Want your competition featured on the website? Have a question to ask? Send us a message.</p>
+                    <h3 class="homepage-tips-title">User Guide</h3>
+                    <p class="homepage-tips-description">New to WOKO? As an APU student, you'll be glad to know that you can join. Here's how.</p>
                 </div>
             </li>
-            <li class="homepage-contact-item">
+            </a>
+            
+            <a href="about.php" class="homepage-tips-item">
+            <li>
                 <hr class="homepage-line">
                 <div>
-                    <h3 class="homepage-contact-title">&lt; About Us ></h3>
-                    <p class="homepage-contact-description">What is WOKO? Does it even stand for anything? Read more about the platform over here.</p>
+                    <h3 class="homepage-tips-title">About Us</h3>
+                    <p class="homepage-tips-description">What is WOKO? Does it even stand for anything? Read more about the platform over here.</p>
                 </div>
             </li>
+            </a>
         </ul>
     </div>
+</div>
+<br><br>
+<div class="container homepage-cards">
 
-    <?php
+    <div class="row">
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="assets/images/me.JPG" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Jaedonothon</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="assets/images/me.JPG" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Jaedonothon</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="assets/images/me.JPG" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Jaedonothon</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="assets/images/me.JPG" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Jaedonothon</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="assets/images/me.JPG" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Jaedonothon</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card" style="width: 20rem;">
+                <img class="card-img-top" src="assets/images/me.JPG" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Jaedonothon</h4>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br>
+
+</div>
+
+
+<?php
 
 include 'misc/footer.php';
 
